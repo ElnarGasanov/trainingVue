d6c1e95b30ad54ada9806c12936cb3eeb66e6654
Elnar Gasanov <elnargasanov1@mail.ru>
2021-11-22 04:33:16 +0300

    vue commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,77 @@
+<!--корневое приложение APP-->
+
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <!--ЗДЕСЬ МЫ ПИШЕМ РАЗМЕТКУ-->
+  <main class="app">
+    <form>
+      <h4>Создание поста</h4>
+      <input class="input" type="text" placeholder="Название">
+      <input class="input" type="text" placeholder="Описание">
+      <button class="btn" @click="createPost">Создать пост</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong>{{ post.title }}</div>
+      <div><strong>Описание:</strong>{{ post.body }}</div>
+    </div>
+  </main>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+//ЗДЕСЬ описываем логику, создаём функции, объявляем данные
+//важный момент в этой секции по default мы должны экспортировать объект{}
+export default {
+  data() {
+    return {
+      posts: [
+        {id: 1, title: "JavaScript 1", body: "Описание поста 1"},
+        {id: 2, title: "JavaScript 2", body: "Описание поста 2"},
+        {id: 3, title: "JavaScript 3", body: "Описание поста 3"},
+      ],
+    }
+  },
+  methods: {
+    createPost:{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+.app{
+  padding: 20px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.post {
+  margin-top: 15px;
+  padding: 15px;
+  border: 2px solid teal;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+form{
+  display: flex;
+  flex-direction: column;
+}
+
+.input{
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn{
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 20px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+  cursor: pointer;
 }
 </style>
